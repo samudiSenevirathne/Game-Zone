c61248797fedb77495cb1d922485c6ae71e4a2d1
samudiSenevirathne <stsamudi@gmail.com>
2023-10-03 00:20:38 +0530

    level One Completed

--- myScript.js
+++ myScript.js
@@ -1,28 +1,41 @@
+
+let count=0;
+let id;
+
 playGame ();
 
 function playGame () {
 
-    let count=0;
+    count=0;
 
     $("#title").fadeOut(6000);
 
-    setInterval(function(){
-        count++;
-        $("#marks>h1").text(count);
-    },6000);
+    id=setInterval(function marks(){
+        $("#marks>h1").text(count++);
 
-    // var dset;
-    // var dw;
-    // var dh;
-    // var t1set;
-    // var t2set;
-    // var t3set;
-    // var t1w;
-    // var t1h;
-    // var t2w;
-    // var t2h;
-    // var t3w;
-    // var t3h;
+
+        /*level 01 complete*/
+        if(count==6) {
+            $("#road").css("animation-play-state", "paused");
+            $("#tree1").css("animation-play-state", "paused");
+            $("#tree2").css("animation-play-state", "paused");
+            $("#tree3").css("animation-play-state", "paused");
+            $("#restart").css("visibility", "visible");
+            $("#restart>h1").text("level 01 complete").css({"left": "502px", "top": "265px", "visibility": "visible"});
+            $("#title").css("animation-name", "paused").text('   ');
+            $("#restart").fadeOut(6000);
+            // clearInterval(id);
+        }
+        /*level 02 start*/
+        if(count>6){
+            $("#road").css({"animation-duration":"25s","animation-play-state":"running"});
+            $("#dragon").css("border", "1px solid yellow");
+            $("#tree1").css("animation-play-state","running");
+            $("#tree2").css("animation-play-state", "running");
+            $("#tree3").css("animation-play-state", "running");
+        }
+
+    },6000);
 
 
     var dw= $("#dragon").width();
@@ -37,7 +50,7 @@ function playGame () {
     var t3w= $("#tree3").width();
     var t3h= $("#tree3").height();
 
-        setInterval(function(){
+    setInterval(function() {
 
             var dset= $("#dragon").offset();
 
@@ -45,18 +58,20 @@ function playGame () {
             var t2set= $("#tree2").offset();
             var t3set= $("#tree3").offset();
 
-    if(dset.left < t1set.left + t1w && dset.left + dw > t1set.left && dset.top < t1set.top + t1h && dset.top + dh > t1set.top || dset.left < t2set.left + t2w && dset.left + dw > t2set.left && dset.top < t2set.top + t2h && dset.top + dh > t2set.top || dset.left < t3set.left + t3w && dset.left + dw > t3set.left && dset.top < t3set.top + t3h && dset.top + dh > t3set.top ) {
-        $("#road").css("animation-play-state", "paused");
-        $("#tree1").css("animation-play-state", "paused");
-        $("#tree2").css("animation-play-state", "paused");
-        $("#tree3").css("animation-play-state", "paused");
-        $("#restart").css("background", "rgba(43, 41, 41, 0.81)");
-        $("#restart>button").css("visibility","visible");
-        count=0;
-        $("#title").css("animation-play-state", "paused").text('Game Over').fadeIn(100);
-        clearInterval();
-    }
-        },100);
+      if(dset.left < t1set.left + t1w && dset.left + dw > t1set.left && dset.top < t1set.top + t1h && dset.top + dh > t1set.top || dset.left < t2set.left + t2w && dset.left + dw > t2set.left && dset.top < t2set.top + t2h && dset.top + dh > t2set.top || dset.left < t3set.left + t3w && dset.left + dw > t3set.left && dset.top < t3set.top + t3h && dset.top + dh > t3set.top ) {
+          $("#road").css("animation-play-state", "paused");
+          $("#tree1").css("animation-play-state", "paused");
+          $("#tree2").css("animation-play-state", "paused");
+          $("#tree3").css("animation-play-state", "paused");
+          $("#restart").fadeIn(100).css("visibility", "visible");
+          $("#restart>h1").text("Fail").css({"left": "610px", "top": "350px", "visibility": "visible"});
+          $("#restart>button").css("visibility", "visible");
+          clearInterval(id);
+          count = 0;
+          $("#title").css("animation-name", "paused").text('Game Over').fadeIn(100);
+
+      }
+    },100);
 
 }
 
@@ -72,7 +87,7 @@ $("#restart>button").click(function() {
 //     $("#tree1").css("animation-play-state", "paused");
 //     $("#tree2").css("animation-play-state", "paused");
 //     $("#tree3").css("animation-play-state", "paused");
-//     $("#restart").css("background", "rgba(43, 41, 41, 0.81)");
+//     $("#restart").css("visibility", "visible");
 //     $("#restart>button").css("visibility","visible");
 //     $("#restart>button").text("play");
 //     $("#title").css("animation-play-state", "paused").text('paused').fadeIn(100);
